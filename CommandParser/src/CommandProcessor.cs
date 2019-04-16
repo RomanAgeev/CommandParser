a437bc82d7dd58a6d05732c284a4170b272f2a06
Roman Ageev <roman.ageyev@devexpress.com>
2019-04-15 22:00:36 -0400

    add source code and dependencies

---
+++ CommandParser/src/CommandProcessor.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ParallelZipNet.Commands {
+    public class CommandProcessor {
+        readonly List<Command> commands = new List<Command>();
+
+        public Command Register(Action<IEnumerable<Option>> action) {
+            var command = new Command(action);
+            commands.Add(command);
+            return command;
+        }
+
+        public Action Parse(params string[] args) {
+            foreach(var command in commands) {                
+                if(command.TryParse(args, out Action action))
+                    return action;
+            }
+            throw new UnknownCommandException();            
+        }
+    }
+}
